22edb85d3b74de206c761215dcefcae7d61cee85
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-11-16 23:45:27 +0200

    Use SQLite.

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var (
-	logger log.Logger
+	logger     log.Logger
+	db_handler *sql.DB
 )
 
 func init() {
+	// initialize the logger
+	var err error
 	if _, err := os.Stat("logs.txt"); os.IsNotExist(err) {
 		os.Create("logs.txt")
 	}
@@ -20,14 +26,20 @@ func init() {
 		log.Fatal("Could not open logs.txt")
 	}
 	var logger = log.New(file, "PROXY_LOG", log.LstdFlags)
-	fmt.Println("done with %s", logger)
+	fmt.Fprintf(os.Stdout, "%+v\n", logger)
+
+	// initialize the database
+	db_handler, err = sql.Open("sqlite3", "meta.db")
+	if err != nil {
+		logger.Fatal("Not able to create Databse")
+	}
 }
 
 func ReceiveRideRequest(w http.ResponseWriter, req *http.Request) {
-
 	// We need to generate an ID for this one
+	// Id should be loaded from the persistent database
 	// var id int = 0
-	// var foo = log.New(out io.Writer, prefix string, flag int)
+	// log.Printf("this is the message %v", )
 
 	// receive the rest of the request
 	// store the (ID, Status) pair in the DB (use persistent store)
