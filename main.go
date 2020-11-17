86ca3d6fd2266f93ee2de6911599b4dbc9d929b5
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-11-17 02:14:28 +0200

    Remove excessive logic.

--- main.go
+++ main.go
@@ -35,66 +35,19 @@ func init() {
 	}
 }
 
+/*
+ * POST method
+ * http://ServiceNameForProxy/Api[/Api]*?fwd=FwdToKwd[&ApiParam]*
+ */
+
 func ReceiveRideRequest(w http.ResponseWriter, req *http.Request) {
-	// We need to generate an ID for this one
-	// Id should be loaded from the persistent database
-	// var id int = 0
-	// log.Printf("this is the message %v", )
+	if req.Method != "POST" {
 
-	// receive the rest of the request
-	// store the (ID, Status) pair in the DB (use persistent store)
-	// and pass it to TripManger
-}
-
-func receive_trip_instance(w http.ResponseWriter, req *http.Request) {
-	/*
-		fmt.Println("-")
-		fmt.Println(req.URL.Query())
-		request_id, err := strconv.ParseInt(req.URL.Query().Get("request_id"), 10, 64)
-		if err != nil {
-			fmt.Println("Cannot parse request_id")
-			return
-		}
-		client_id, err := strconv.ParseInt(req.URL.Query().Get("client_id"), 10, 64)
-		if err != nil {
-			fmt.Println("Cannot parse client_id")
-			return
-		}
-		trip_id, err := strconv.ParseInt(req.URL.Query().Get("trip_id"), 10, 64)
-		if err != nil {
-			fmt.Println("Cannot parse trip_id")
-			return
-		}
-		driver_id, err := strconv.ParseInt(req.URL.Query().Get("driver_id"), 10, 64)
-		if err != nil {
-			fmt.Println("Cannot parse driver_id")
-			return
-		}
-		timestamp_unix_time, err := strconv.ParseInt(req.URL.Query().Get("timestamp"), 10, 64)
-		timestamp := time.Unix(timestamp_unix_time, 0)
-		if err != nil {
-			fmt.Println("Cannot parse timestamp")
-			return
-		}
-		fwd_to := req.URL.Query().Get("fwd_to")
-		if err != nil {
-			fmt.Println("Error")
-			return
-		}
-	*/
-	/*
-		fmt.Println(request_id)
-		fmt.Println(client_id)
-		fmt.Println(trip_id)
-		fmt.Println(driver_id)
-		fmt.Println(timestamp)
-		fmt.Fprintln(os.Stdout, "- Forward to %s", fwd_to)
-	*/
-	// http.Get(fmt.Sprintf("%s", service_url))
+	}
 }
 
 func main() {
-	http.HandleFunc("/trip", ReceiveRideRequest)
+	http.HandleFunc("/user", ReceiveRideRequest)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("ListenAndServe did not work")
